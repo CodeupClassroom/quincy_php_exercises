115adcb4af7285d48fd71852ffe4090301ecca22
Zach Gulde <zachgulde@gmail.com>
2017-07-19 10:30:52 -0500

    finish do while loop exercise

--- while.php
+++ while.php
@@ -17,9 +17,21 @@ $test = 5;
 // Create a do-while loop that will count by 2's starting with 0 and ending at
 // 100. Follow each number with a newline.
 
-$a = 0;
+// Alter your loop to count backwards by 5's from 100 to -10.
+
+$a = 100;
+
+/* do { */
+/*     echo $a . PHP_EOL; */
+/*     $a -= 5; */
+/* } while ($a >= -10); */
+
+// Create a do-while loop that starts at 2, and displays the result $a * $a on
+// each line while $a is less than 1,000,000. Output should equal:
+
+$a = 2;
 
 do {
     echo $a . PHP_EOL;
-    $a += 2;
-} while ($a <= 100);
+    $a *= $a;
+} while ($a < 1000000);
