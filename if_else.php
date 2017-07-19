4e9d36303a5c9e07132d1f56edaec131fc17b24a
Zach Gulde <zachgulde@gmail.com>
2017-07-19 10:12:54 -0500

    finish if else exercise

--- if_else.php
+++ if_else.php
@@ -7,29 +7,26 @@ $c = '10';
 // TODO: Shorten these two if statements to a single if/else
 if ($a < $b) {
     echo "$a is less than $b\n";
-}
-if ($a >= $b) {
+} else {
     echo "$a is greater than or equal to $b\n";
 }
+
+echo $b < $c ? "$b < $c\n" : "$b >= $c\n";
 
 // TODO: Shorten these two if statements to a single if/else
 if ($b < $c) {
     echo "$b is less than $c\n";
-}
-if ($b >= $c) {
+} else {
     echo "$b is greater than or equal to $c\n";
 }
-
 // TODO:
 // combine the next 3 conditionals into one
 // if/elseif/else block that checks in order for:
 // identical, equal, not equal/identical
 if ($b === $c) {
     echo "$b is identical to $c\n";
-}
-if ($b == $c) {
+} else if ($b == $c) {
     echo "$b is equal to $c\n";
-}
-if ($b != $c) {
+} else {
     echo "$b is not equal to $c\n";
 }
