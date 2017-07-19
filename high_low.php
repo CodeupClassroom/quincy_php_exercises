5ab6f3d504cd371bc9036292a947bcae1d5abb3e
Zach Gulde <zachgulde@gmail.com>
2017-07-19 14:18:07 -0500

    finish high low game

--- high_low.php
+++ high_low.php
@@ -7,9 +7,28 @@ echo "The secret number is: $secretNumber!" . PHP_EOL;
 // LOOP
 do {
     // prompts user to guess the number
+    echo 'What\'s your guess? ';
+    $userGuess = trim(fgets(STDIN));
+
+    // validate user input
+    if (! is_numeric($userGuess)) {
+        echo 'Hey!!! That\'s not a number!' . PHP_EOL;
+        continue;
+    }
+
     // if user's guess is less than the number, it outputs "HIGHER"
+    if ($userGuess < $secretNumber) {
+        echo 'Higher!' . PHP_EOL;
+    }
     // if user's guess is more than the number, it outputs "LOWER"
+    if ($userGuess > $secretNumber) {
+        echo 'Lower!' . PHP_EOL;
+    }
     // if a user guesses the number, the game should declare "GOOD GUESS!"
-} while (/* the guess is not equal to the random number */);
+    if ($userGuess == $secretNumber) {
+        echo 'Good Guess!' . PHP_EOL;
+    }
+
+} while ($userGuess != $secretNumber);
 
 
