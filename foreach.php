8b8d758358dd469c6e3c4f74dbae363a3518e711
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-07-21 13:44:35 -0500

    change tab to space

--- foreach.php
+++ foreach.php
@@ -44,7 +44,7 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 foreach($things as $thing) {
     if(is_array($thing)) {
         foreach($thing as $element) {
-            echo $element . "\t";
+            echo $element . " ";
         }
     } else {
         echo $thing . PHP_EOL;
